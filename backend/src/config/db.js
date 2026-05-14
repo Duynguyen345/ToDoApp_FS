5c495a413dcaead1533118f0d8bd30992a68be9d
DuyNguyenCoder <nduy52042@gmail.com>
2026-05-14 21:26:59 +0700

    14-5-2026 l1

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,13 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000,
-      family: 4,
-    });
-    console.log("Kết nối MongoDB thành công! 🎉");
-  } catch (error) {
-    console.error("Lỗi kết nối MongoDB:", error.message);
-  }
-};
+// export const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       serverSelectionTimeoutMS: 5000,
+//       family: 4,
+//     });
+//     console.log("Kết nối MongoDB thành công! 🎉");
+//   } catch (error) {
+//     console.error("Lỗi kết nối MongoDB:", error.message);
+//   }
+// };
